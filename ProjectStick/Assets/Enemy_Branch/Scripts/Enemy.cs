288ed4504c09f2b38bb6ca5b2ec035f3aa00d54a
icetea1110 <s24033@gsm.hs.kr>
2025-04-22 20:48:35 +0900

    스크립트만 생성함

--- ProjectStick/Assets/Enemy_Branch/Scripts/Enemy.cs
+++ ProjectStick/Assets/Enemy_Branch/Scripts/Enemy.cs
@@ -6,6 +6,12 @@ public class Enemy : MonoBehaviour, IEnemyDamage
 {
     public int Hp;
     public int attackAmount;
+    public enum EnemyKind
+    {
+        Normal = 0,
+        Boss = 1
+    }
+    public EnemyKind Kind;
 
     public virtual void Damage(float damage)
     {
