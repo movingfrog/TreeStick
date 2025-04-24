115758abd452f91700d271b72c13f540d2e7216b
movingfrog <s24047@gsm.hs.kr>
2025-04-24 20:04:39 +0900

    일단 벽점프 완성

--- ProjectStick/Assets/Player/Scripts/PlayerMove.cs
+++ ProjectStick/Assets/Player/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,9 +8,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float JumpPower = 10f;
     [SerializeField] private float Distance = 0.01f;
     [SerializeField] private Vector2 Direction = new Vector2(0,-0.45f);
+    [SerializeField] private Vector2 WallDirection;
     [SerializeField] private LayerMask layer;
+    [SerializeField] private bool isGround;
+    [SerializeField] private bool isWall;
+    bool canMove = true;
     private float moveInput; // 왼쪽/오른쪽 입력만
-    private bool isGround;
+    
 
     private Rigidbody2D rb;
 
@@ -22,21 +27,76 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<float>();
+        if (Input.anyKey)
+        {
+            transform.localScale = new Vector3(moveInput == 0 ? transform.localScale.x : moveInput > 0 ? 1 : -1, 1, 1);
+        }
     }
 
+    //벽 점프를 넣을 것이라면 벽과 벽사이를 6칸 뛰어놓아야한다
     public void OnJump()
     {
-        if(isGround && Input.anyKeyDown)
+        if(Input.anyKeyDown)
         {
-            rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            if (isGround)
+            {
+                rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+                Debug.Log("jump");
+            }
+            else
+            {
+                if (isWall)
+                {
+                    rb.velocity = Vector2.zero;
+                    WallJump();
+                    Debug.Log("Success");
+                }
+            }
+        }
+    }
+
+    void isSliding()
+    {
+        if(!isGround && isWall && rb.velocity.y < 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -1f);
+        }
+    }
+
+    void WallJump()
+    {
+        float walljump = -transform.localScale.x;
+        StartCoroutine(wallJump(walljump * (JumpPower + 5), JumpPower + 1.5f));
+        Debug.Log(rb.velocity);
+
+        transform.localScale = new Vector3(walljump, 1, 1);
+
+        isWall = false;
+        canMove = false;
+        StartCoroutine(WaitAction.wait(0.15f, () => canMove = true));
+    }
+
+    IEnumerator wallJump(float x, float y)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            rb.velocity = new Vector2(x, y) * 1.5f;
+            yield return new WaitForSeconds(0.02f);
         }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        if(canMove)
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        }
+        Bounds bounds = GetComponent<Collider2D>().bounds;
         RaycastHit2D hit = Physics2D.Raycast(rb.position + Direction, Vector2.down * Distance, Distance, layer);
+        isWall = Physics2D.BoxCast(transform.position, bounds.size, 0f, Vector2.right * transform.localScale.x * Direction, Distance, layer);
         isGround = hit.collider != null;
+        isSliding();
+        Debug.Log(moveInput);
     }
 
     private void OnDrawGizmos()
