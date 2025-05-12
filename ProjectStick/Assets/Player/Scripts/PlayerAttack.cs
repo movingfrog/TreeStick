1f6f86ccb36a39cf1bd14510309efb0dc263accb
movingfrog <s24047@gsm.hs.kr>
2025-05-12 21:16:19 +0900

    playerattackdefault

--- ProjectStick/Assets/Player/Scripts/PlayerAttack.cs
+++ ProjectStick/Assets/Player/Scripts/PlayerAttack.cs
@@ -3,6 +3,41 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public float attackDelay;
+    public float attackSec;
 
+    public Vector2 distance;
+    public Vector2 attackSize;
 
+    void OnAttack()
+    {
+        if(attackSec >= attackDelay && Input.anyKeyDown)
+        {
+            Collider2D[] enemy = Physics2D.OverlapBoxAll(transform.position + new Vector3(distance.x * transform.localScale.x, distance.y), attackSize, 0);
+            foreach(var collider in enemy)
+            {
+                if (collider.CompareTag("Enemy"))
+                {
+                    IEnemyDamage Attack = collider.GetComponent<IEnemyDamage>();
+                    //Attack.Damage();
+                    continue;
+                }
+                attackSec = 0;
+                break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(attackSec < attackDelay)
+        {
+            attackSec += Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position + new Vector3(distance.x * transform.localScale.x, distance.y), attackSize);
+    }
 }
